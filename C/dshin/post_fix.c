2f94550f4e823d227f1fa435d41b41f8d1e0fd45
comeeasy <zz1236zz@naver.com>
2020-05-01 15:18:56 +0900

    infic -> postfix

---
+++ C/dshin/post_fix.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct stack {
+    char stack[MAX];
+    int peek;
+} Stack;
+
+Stack* stack_init(Stack* s) {
+        s = (Stack*)malloc(sizeof(Stack));
+        s->peek = 0;
+
+        return s;
+    }
+int stack_is_empty(Stack* s) {return (s->peek == -1);}
+int stack_is_full(Stack* s) {return (s->peek == MAX-1);}
+void push(Stack* s, char ch) {
+    if(!stack_is_full(s)) {
+        s->stack[++(s->peek)] = ch;
+    }
+    else {
+        printf("stack is full!!\n\n");
+    }
+}
+char pop(Stack* s) {
+    if(!stack_is_empty(s)) {
+        return s->stack[(s->peek)--];
+    }
+    else {
+        printf("stack is empty!\n\n");
+    }
+}
+char top(Stack* s) {
+        if(!stack_is_empty(s))  return (s->stack[s->peek]);
+        else return '\0';
+    }
+int compare_priority(char op1, char op2) {
+    /*
+        if op1's priority is higher than op2 
+            then return 1
+        else if (opposit case)
+            then return 0
+        else   (p1 == p2)
+            then return 1
+    */
+
+    int p1, p2;
+    if(op1 == '\0') return 0;
+
+    if(op1 == '*' || op1 == '/') {p1 = 1;}
+    else {p1 = 2;}
+    if(op2 == '*' || op2 == '/') {p2 = 1;}
+    else {p2 = 2;}
+
+    if( p1 > p2 )       return 0;
+    else if( p2 > p1 )  return 1;
+    else                return 1; 
+}
+
+int main() {
+    Stack* s;
+    char exp[MAX], ch;
+
+    s = stack_init(s);
+    printf("enter infix exp: ");
+    scanf("%s", exp);
+
+    for(int i=0; i<strlen(exp); ++i) {
+        ch = exp[i];
+
+        // operator 
+        if(ch=='*' || ch=='+' || ch=='-' || ch=='/') {
+            while(compare_priority(top(s), ch)) {
+                printf("%c", top(s));
+                pop(s);
+            }
+            push(s, ch);
+        } // operand
+        else if(ch =='(') {
+            push(s, ch);
+        }
+        else if(ch == ')') {
+            while(top(s) != '(') {
+                printf("%c", top(s));
+                pop(s);    
+            }
+        }
+        else {
+            printf("%c", ch);
+        }
+    }
+    while(!stack_is_empty(s)) {
+        printf("%c", top(s));
+        pop(s);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+
---
+++ C/dshin/win_prac.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello wsl\n");
+    return 0;
+}
