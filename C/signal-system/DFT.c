31f27234b2df768b3c5939cb03bd8e5b66d12899
comeeasy <zz1236zz@naver.com>
2020-06-18 16:43:30 +0900

    o

--- C/signal-system/DFT.c
+++ C/signal-system/DFT.c
@@ -6,10 +6,10 @@ int main() {
     sample = init_sample(sample);
     sample = get_sample(sample);
 
-    //window(sample, 100);
+    //window(sample, 4000);
     window(sample, 500);
-    window(sample, 1000);
-    window(sample, 2000);
+    //window(sample, 1000);
+    //window(sample, 2000);
 
     return 0;
 }
