cd81f97880fb108053d6cd85240652bb0eb10a8a
comeeasy <zz1236zz@naver.com>
2020-06-18 20:11:15 +0900

    hw08 done

--- C/dshin/hw08/merge.c
+++ C/dshin/hw08/merge.c
@@ -5,12 +5,20 @@
 #include "merge.h"
 
 // =================================================
+/*
+    왼쪽이나 오른쪽 array에 비교할 값들이 남아있다면 하나씩 값들을
+    비교하여 copy array에 저장하는 것이 맞지만 그렇지 않다면 남은 값들을
+    하나씩 넣어주면 된다.
+*/
 
 int select_func(int list[], int* compare_left, int* compare_right, int mid, int right) {
+    // 왼쪽과 오른쪽 모두 비교할 값들이 남아있다면,
     if( *compare_left <= mid && *compare_right <= right ) {
+        // 비교하여 적절한 값 return
         if(list[*compare_left] <= list[*compare_right]) return list[(*compare_left)++];
         else                                            return list[(*compare_right)++]; 
     }
+    // 아니라면 둘 중에 남아있는 값들을 비교하지 않고 줄줄이 return
     else {
         if(*compare_left > mid) return list[(*compare_right)++];
         else                    return list[(*compare_left)++];
@@ -19,21 +27,25 @@ int select_func(int list[], int* compare_left, int* compare_right, int mid, int 
 
 void merge(int list[], int left, int mid, int right) {
     // size of list
+    // 왼쪽 array와 오른쪽 array의 크기를 합친 크기만큼만
+    // 비교하여 새로운 배열에 순서대로 저장할 것이므로 이를 n이라 하자
     int n = right - left + 1;
     // array for sorted
+    // 정렬된 값들을 저장할 array
     int* copy = (int*)malloc(sizeof(int)*n);
     // 비교하면서 하나씩 커질 indeics
+    // 왼쪽에서 값을 비교할 compare_left
+    // 오른쪽에서 값을 비교할 compare_right을 선언한다.
     int compare_left = left, compare_right = mid + 1;
 
-    for(int i=0; i<n; ++i) {
+    for(int i=0; i<n; ++i)
+        // 왼쪽과 오른쪽 array들의 값을 하나씩 비교하여 순서대로 copy array
+        // 에 저장한다. 
         copy[i] = select_func(list, &compare_left, &compare_right, mid, right);
-    }
-    for(int i=0; i<n; ++i) {
+    for(int i=0; i<n; ++i)
+        // 다시 원래 list로 저장한 값들을 복사한다. 
         list[i + left] = copy[i];
-    }
     
     free(copy);
 }
-
-
 // =================================================
--- C/dshin/hw08/quick.c
+++ C/dshin/hw08/quick.c
@@ -5,24 +5,28 @@
 #include "quick.h"
 
 // ==============================================================
-
 int partition(int list[], int left, int right) {
+    // list의 가장 왼쪽에 있는 값을 pivot값으로 설정한다.
     int pivot = list[left];
+    // low, high를 각각 compare_left, compare_right이라 하였고
+    // 아래와 같이 초기화 하였다.
     int compare_left = left+1, compare_right = right, tmp;
 
     while(1) {
-        //for(int i=left; i<=right; ++i) printf("%d ", list[i]); printf("\n");
-
+        // 바꿔야할 수도 있는 index를 왼쪽에서부터 탐색한 후 대기
         while( list[compare_left]  <= pivot && compare_left <= right ) ++compare_left;
+        // 위와 마찬가지이다.
         while( list[compare_right] >  pivot && compare_right >= left ) --compare_right;
+        // 만약 low, high가 크로스 되었다면 왼쪽 array와 오른쪽 array가 구분 된 것이므로 break
         if(compare_left > compare_right) break;
+        // 그렇지 않다면 두 값은 서로 바뀌어야하므로 swap
         SWAP(list[compare_left], list[compare_right], tmp);
-
-        //printf("swaq %d %d\n", list[compare_right], list[compare_left]);
     }
-    SWAP(list[left], list[compare_right], tmp);
 
+    // while문을 탈출했다는 것은 왼쪽과 오른쪽이 구별 되었다는 것이므로
+    // pivot값을 그 사이에 넣어주고 high를 return한다. 이유는 위에서
+    // 위에서 설명하였던것처럼 high가 pivot이 위치하기 적절하기 때문이다.
+    SWAP(list[left], list[compare_right], tmp);
     return compare_right;
 }
-
 // ==============================================================
