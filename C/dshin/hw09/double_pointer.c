51b0ed4338947f7d9f195f6926e9e752270d36d1
comeeasy <zz1236zz@naver.com>
2020-06-24 14:20:28 +0900

    double-pointer

--- C/dshin/hw09/double_pointer.c
+++ C/dshin/hw09/double_pointer.c
@@ -6,6 +6,10 @@
 
 // ==========================================================
 
+#define SWAP(x, y, t)   ((t) = (x)), ((x) = (y)), ((y) = (t))
+
+// ==========================================================
+
 typedef struct _element {
     int key;
     char word[WORD_SIZE];
@@ -13,55 +17,53 @@ typedef struct _element {
 
 // ==========================================================
 
-void swap(element*** a, element*** b) {
-    element** tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-int partition(element* sorted_pointer[], int low, int high) {
-    int pivot = *(words+sorted_index[low])->key;
-    int left = low+1, right = high;
+int partition(element* words[], int low, int high) {
+    //int pivot = *(words+sorted_index[low])->key;
+    int pivot = (words[low])->key;
+    int left  = low+1, right = high;
+    element* tmp;
 
     while(1) {
-        while( (words+sorted_index[left])->key  <= pivot && left <= high ) ++left;
-        while( (words+sorted_index[right])->key >  pivot && right >= low ) --right;
+        while( (words[left])->key  <= pivot && left <= high ) ++left;
+        while( (words[right])->key >  pivot && right >= low ) --right;
         if(left > right) break;
-        swap( &sorted_pointer[left] , &sorted_pointer[right] );
+        SWAP(words[left], words[right], tmp);
     }
 
-    swap( &sorted_pointer[low], &sorted_pointer[right]);
+    SWAP(words[low], words[right], tmp);
     return right;
 }
 
-void quick_sort(element* words, int sorted_index[], int low, int high) {
+void quick_sort(element* words[], int low, int high) {
     if (low < high) { 
-        int q = partition(words, sorted_index, low, high);
-        quick_sort(words, sorted_index, low, q - 1); 
-        quick_sort(words, sorted_index, q + 1, high); 
+        int q = partition(words, low, high);
+        quick_sort(words, low, q - 1); 
+        quick_sort(words, q + 1, high); 
     }  
 }   
 
-FILE* get_dictionary(FILE* fp, element* words, int* file_size) {
+FILE* get_dictionary(FILE* fp, element* words[], int* file_size) {
     if( !(fp = fopen(DICT_IN, "r")) )  {
             printf("file is not open\n");
             return NULL;
         }
 
-        for(int i=0; i<DICT_SIZE; ++i) {
-            fscanf(fp, "%d", &((words+i)->key));
-            fscanf(fp, "%s", (words+i)->word);
-            if( (words+i)->key == 0 ) {
-                *file_size = i;
-                break;
-            }
-        }
+    for(int i=0; i<DICT_SIZE; ++i) {
+        words[i] = (element*)malloc(sizeof(element));
 
-        printf("file size = %d\n", *file_size-1);
-        return fp;
+        fscanf(fp, "%d", &(words[i])->key);
+        fscanf(fp, "%s", (words[i])->word);
+       if( (words[i])->key == 0 ) {
+            *file_size = i;
+            break;
+        }
+    }
+
+    printf("file size = %d\n", *file_size);
+    return fp;
 }
 
-FILE* write_dictionary(FILE* fp, element* words, int sorted_index[], int file_size) {
+FILE* write_dictionary(FILE* fp, element* words[], int file_size) {
     fp = fopen(DICT_OUT, "w");
 
     if(fp == NULL) {
@@ -69,32 +71,35 @@ FILE* write_dictionary(FILE* fp, element* words, int sorted_index[], int file_si
     }
 
     for(int i=0; i<file_size; ++i) {
-        fprintf(fp, "%d %s\n", (words+sorted_index[i])->key, (words+sorted_index[i])->word);
+        fprintf(fp, "%d %s\n", (words[i])->key, (words[i])->word);
     }
 
     return fp;
 }
 
+void delete_elements(element* words[], int file_size) {
+    for(int i=0; i<=file_size; ++i) {
+        free(words[i]);
+    }
+}
+
 int main() {
     FILE* fp;
     int file_size;
-    element* words;
-
-    // init words, sorted_index
-    words = (element*)malloc(sizeof(element) * DICT_SIZE);
-    for(int i=0; i<DICT_SIZE; ++i) sorted_pointer[i] = (word+i);
+    element* words[DICT_SIZE];
 
     // get unsorted dictionary
     fp = get_dictionary(fp, words, &file_size);
     if(fp == NULL) return EXIT_FAILURE;
 
     // sort
-    quick_sort(words, sorted_pointer, 0, file_size-1);
-
+    quick_sort(words, 0, file_size-1);
+    
     // write dictionary
-    fp = write_dictionary(fp, words, sorted_pointer, file_size);
+    fp = write_dictionary(fp, words, file_size);
+    
 
+    delete_elements(words, file_size);
     fclose(fp);
-    free(words);
     return 0;
 }
