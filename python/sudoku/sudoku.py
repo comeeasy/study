6e6e03e08a35abb382732d649532fbac6a3f243e
comeeasy <zz1236zz@naver.com>
2020-08-27 01:16:40 +0900

    sudoku 백트래킹으로 후보가 2개 이상 있는 경우도 풀 수 있게 알고리즘 짜보자

--- python/sudoku/sudoku.py
+++ python/sudoku/sudoku.py
@@ -1,10 +1,5 @@
 # sudoku.py
 
-'''
-note.ipynb
-에 프로그램에 대한 설명을 기술함
-'''
-
 import sys
 sys.stdin = open('sample.txt', 'r')
 
@@ -25,38 +20,84 @@ def three_by_three_matrix(sudoku, row, column) :
     startRowIdx    = (row // 3) * 3
     startColumnIdx = (column // 3) * 3
     
-    return [sudoku[row][column] for row in range(startRowIdx, startRowIdx+3)
+    return [sudoku[row][column].number for row in range(startRowIdx, startRowIdx+3)
                            for column in range(startColumnIdx, startColumnIdx+3)]
 
 # 해당 loc의 column, row의 list들을 return
 def column_list(sudoku, column) :
-    return [sudoku[row][column] for row in range(N)]
+    return [sudoku[row][column].number for row in range(N)]
 
 def row_list(sudoku, row) :
-    return [sudoku[row][column] for column in range(N)]
+    return [sudoku[row][column].number for column in range(N)]
 
 # sudoku에서 빈 loc들의 list를 
-def get_empty_loc(sudoku)
+def get_empty_loc(sudoku) :
+    toRet = []
+    for row in range(N) :
+        for column in range(N) :
+            if sudoku[row][column].number == 0 :
+                toRet.append(sudoku[row][column])
+    
+    return toRet
 
+# 비어있는 loc들을 받아서 각 loc의 3*3, column, row list들을 받아서 후보들을 지정한다
+def get_candidate(sudoku) :
+    emptyList = get_empty_loc(sudoku)
+    
+    for loc in emptyList :
+        existNumbers = sorted(list(set(three_by_three_matrix(sudoku, loc.row, loc.column) + 
+                           column_list(sudoku, loc.column) + 
+                           row_list(sudoku, loc.row))))
+        existNumbers.remove(0)
+        
+        loc.candidate = [num for num in range(1, 10) if num not in existNumbers]
+        print('({0}, {1}), candidate: {2}'.format(loc.row, loc.column, loc.candidate))
+    print()
+        
+# 비어있는 loc이 없다면 모두 찼으므로 게임 종료
+def play_sudoku(sudoku) :
+    while(get_empty_loc(sudoku)) :
+        # 각 loc들에 들어갈 수 있는 숫자들의 후보들을 받아온다
+        get_candidate(sudoku)      
+        
+        # 현재 알고리즘은 단 하나도 단일 후보가 없은 loc이 생기면 무한 루프가 생겨 풀 수 없음
+        # 따라서 그런 경우가 생겼을 때 풀 수 없다고 알리고 함수 종료
+        
+        # 후보가 가장 작은 loc들을 순서대로 불러와서 그 후보들을 차례대로 넣고 백트래킹으로 풀어봐야할거 같다 ㅠㅠ
+        if all([True if len(loc.candidate) > 1 else False for loc in get_empty_loc(sudoku)]) :
+            print('problem is not solvable')
+            return None
+            
+        for loc in get_empty_loc(sudoku) :
+            # 모든 loc.candidate의 크기가 2 이상이라면 당장은 해결할 수 없는 스도쿠이다
+            if len(loc.candidate) == 1 :
+                loc.number = loc.candidate.pop(0)
+
+def print_sudoku(sudoku) :
+    for row in range(N) :
+        print(sudoku[row])
+
+# main #######################################################################################
 # 스도쿠의 크기      
 N = 9
 
-# sudoku 입력
-sudoku = []
-for row in range(N) :
-    ithRow = list(map(int, list(input())))
-    # 스도쿠에 loc들을 삽입
-    sudoku.append([loc(row, column, ithRow[column]) for column in range(N)])
- 
-# print input sudoku    
-for row in range(N) :
-    print(sudoku[row])
+T = int(input())
+for testCase in range(1, T+1) :
+    # sudoku 입력
+    sudoku = []
+    for row in range(N) :
+        ithRow = list(map(int, list(input())))
+        # 스도쿠에 loc들을 삽입
+        sudoku.append([loc(row, column, ithRow[column]) for column in range(N)])
 
-'''
-print()
-print(three_by_three_matrix(sudoku, 3, 3), end='\n\n')
-print(column_list(sudoku, 3), end='\n\n')
-print(row_list(sudoku, 5), end='\n\n')
-'''
+    print('=== testCase {0} ==='.format(testCase))
+    print('---------- before -------------')
+    print_sudoku(sudoku)
 
+    play_sudoku(sudoku)
+
+    print('\n---------- after -------------')
+    print_sudoku(sudoku)
+
+#########################################################################################
 
